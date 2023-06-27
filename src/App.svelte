8c146af2339b6cb6ddd117a02ec2a91d666dd908
Liam Neville <lineville@github.com>
2023-06-27 01:13:06 -0700

    clean

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,6 @@
   import 'bulma/bulma.sass'
   import BlackJack from './lib/BlackJack.svelte'
   import Navbar from './lib/Navbar.svelte';
-
-  
-  // import { invoke } from '@tauri-apps/api';
-  // import { onMount } from 'svelte';
-
-  // let message = "";
-
-  // onMount(async () => {
-  //   message = await greet();
-  // })
-  
-  // // Example of passing message from Rust to JS
-  // async function greet(): Promise<string> {
-  //   const message : string = await invoke('greet', { name: 'Lets play' });
-  //   console.log(message);
-  //   return message;
-  // }
 </script>
 
 <main>
