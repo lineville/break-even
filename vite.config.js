d6d9eb8dbe029aab08ad813a9af9b628f45248fe
Liam Neville <lineville@github.com>
2025-01-03 22:31:52 -0500

    upgrade vite

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "src/variables.scss";',
-      },
-    },
-  },
 });
